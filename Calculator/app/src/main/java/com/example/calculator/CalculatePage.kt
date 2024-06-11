d89a70135cc3fdfde0527dbd424d609963d41dc1
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2024-06-11 11:48:46 +0530

    created rectabgle shaped buttons with layer-list

--- Calculator/app/src/main/java/com/example/calculator/CalculatePage.kt
+++ Calculator/app/src/main/java/com/example/calculator/CalculatePage.kt
@@ -1,15 +1,19 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.text.Html
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.calculator.databinding.FragmentCalculatePageBinding
 
 class CalculatePage : Fragment() {
 
+    private lateinit var binding: FragmentCalculatePageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
@@ -17,6 +21,7 @@ class CalculatePage : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calculate_page, container, false)
+        binding = FragmentCalculatePageBinding.inflate(inflater,container,false)
+        return binding.root
     }
 }
