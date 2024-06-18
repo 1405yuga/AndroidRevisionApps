91aac739d73f88e8ff5d03d38332cab3106a67f2
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2024-06-18 16:03:25 +0530

    created viewmodel

---
+++ Calculator/app/src/main/java/com/example/calculator/models/CalculatorViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.calculator.models
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CalculatorViewModel : ViewModel() {
+    private val _input : MutableLiveData<String> = MutableLiveData(null)
+    private val _output : MutableLiveData<Double?> = MutableLiveData()
+    private val _highlightOutput : MutableLiveData<Boolean> = MutableLiveData(false)
+    val input : LiveData<String> get() = _input
+    val output : LiveData<Double?> get() = _output
+    val highlightOutput : LiveData<Boolean> get() = _highlightOutput
+}
