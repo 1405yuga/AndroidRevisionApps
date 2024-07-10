4954e0320fbd4824b05790debdc8a97e552b75eb
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2024-07-10 22:33:30 +0530

    handled when symbols are clicked

--- Calculator/app/src/main/java/com/example/calculator/models/CalculatorViewModel.kt
+++ Calculator/app/src/main/java/com/example/calculator/models/CalculatorViewModel.kt
@@ -5,18 +5,28 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CalculatorViewModel : ViewModel() {
-    private val _input : MutableLiveData<String> = MutableLiveData(null)
-    private val _output : MutableLiveData<Double?> = MutableLiveData()
-    private val _highlightOutput : MutableLiveData<Boolean> = MutableLiveData(false)
-    val input : LiveData<String> get() = _input
-    val output : LiveData<Double?> get() = _output
-    val highlightOutput : LiveData<Boolean> get() = _highlightOutput
+    private val _input: MutableLiveData<String> = MutableLiveData(null)
+    private val _output: MutableLiveData<Double?> = MutableLiveData()
+    private val _highlightOutput: MutableLiveData<Boolean> = MutableLiveData(false)
+    val input: LiveData<String> get() = _input
+    val output: LiveData<Double?> get() = _output
+    val highlightOutput: LiveData<Boolean> get() = _highlightOutput
 
-    private var currentValue :String=""
-    fun setInput(inputText:String){
-        currentValue+=inputText
-        _input.value = currentValue
-        _highlightOutput.value = false
+    private var currentValue: String = ""
+    fun setInput(inputText: String) {
+        if (inputText == "+" || inputText == "-" || inputText == "x" || inputText == "÷") {
+            if (currentValue.isNotEmpty() && currentValue[currentValue.length - 1].isDigit()) {
+                currentValue += inputText
+                _input.value = currentValue
+                _highlightOutput.value = false
+            }
+        } else {
+            currentValue += inputText
+            _input.value = currentValue
+            _highlightOutput.value = false
+        }
+
+
     }
 
     fun clearInput() {
