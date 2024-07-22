57e9db5f592d80bf4d0769fe84dea015c28c1fbd
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2024-07-22 20:21:52 +0530

    created and fragment

--- PotterHead/app/src/main/java/com/example/potterhead/MainActivity.kt
+++ PotterHead/app/src/main/java/com/example/potterhead/MainActivity.kt
@@ -5,12 +5,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.example.potterhead.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var nav : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentLayout)
+        if(navHostFragment!=null) nav = navHostFragment.findNavController()
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return nav.navigateUp() || super.onSupportNavigateUp()
     }
 }
