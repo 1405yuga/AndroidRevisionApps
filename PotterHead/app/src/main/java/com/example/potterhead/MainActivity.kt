8f0d05802b45a611311dcb7cc0ab3186b9a001bd
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2024-07-24 19:59:31 +0530

    code reformating

--- PotterHead/app/src/main/java/com/example/potterhead/MainActivity.kt
+++ PotterHead/app/src/main/java/com/example/potterhead/MainActivity.kt
@@ -1,17 +1,13 @@
 package com.example.potterhead
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
-import com.example.potterhead.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var nav : NavController
+    private lateinit var nav: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentLayout)
-        if(navHostFragment!=null) nav = navHostFragment.findNavController()
+        if (navHostFragment != null) nav = navHostFragment.findNavController()
 
     }
 
