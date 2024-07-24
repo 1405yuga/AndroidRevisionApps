2d042284e602216f44366207beccbfca83fe9923
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2024-07-24 21:44:01 +0530

    print data retrieved in log

--- PotterHead/app/src/main/java/com/example/potterhead/fragments/ViewDataFragment.kt
+++ PotterHead/app/src/main/java/com/example/potterhead/fragments/ViewDataFragment.kt
@@ -1,26 +1,37 @@
 package com.example.potterhead.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.potterhead.R
+import androidx.fragment.app.viewModels
+import com.example.potterhead.databinding.FragmentViewDataBinding
+import com.example.potterhead.entity.SpellViewModel
 
 class ViewDataFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var binding: FragmentViewDataBinding
+    private val viewModel by viewModels<SpellViewModel>()
+    private  val TAG = this.javaClass.name
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_data, container, false)
+        binding = FragmentViewDataBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        viewModel.getSpellsList()
+        viewModel.spellsList.observe(viewLifecycleOwner){
+            Log.d(TAG,"Spells List : $it")
+        }
+
+    }
 }
