59bd84973bf8735726c266f9d12b6d8c72074ef5
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2024-07-25 15:44:48 +0530

    comments for api service

--- PotterHead/app/src/main/java/com/example/potterhead/service/ApiService.kt
+++ PotterHead/app/src/main/java/com/example/potterhead/service/ApiService.kt
@@ -8,16 +8,20 @@ import retrofit2.http.GET
 
 private const val BASE_URL = "https://potterapi-fedeperin.vercel.app/en/"
 
+//to create retrofit instance
 private val retrofitBuilder = Retrofit.Builder()
     .baseUrl(BASE_URL)
     .addConverterFactory(GsonConverterFactory.create())
     .build()
 
+//defines api endpoints
 interface ApiService {
     @GET("spells")
     suspend fun getAllSpells():List<Spell>
 }
 
+//created api object
 object  PotterApiService {
+    //lazy : creates instance only once when accessed or called , n same object reused across app
     val retrofitApiService :ApiService by lazy { retrofitBuilder.create(ApiService::class.java) }
 }
