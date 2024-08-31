8f7696de9cc54c5b6d3f50ab8d7329199d1ecfad
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2024-08-31 10:27:52 +0530

    set on click for number button

--- ListApp/app/src/main/java/com/example/listapp/adapter/NumbersAdapter.kt
+++ ListApp/app/src/main/java/com/example/listapp/adapter/NumbersAdapter.kt
@@ -7,7 +7,9 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.listapp.databinding.CardNumberContainerBinding
 
-class NumbersAdapter() : ListAdapter<Int, NumbersAdapter.NumberViewHolder>(DiffCallBack) {
+class NumbersAdapter(
+    private val onButtonClick: (Int) -> (Unit)
+) : ListAdapter<Int, NumbersAdapter.NumberViewHolder>(DiffCallBack) {
     companion object {
 
         private val DiffCallBack = object : DiffUtil.ItemCallback<Int>() {
@@ -23,10 +25,16 @@ class NumbersAdapter() : ListAdapter<Int, NumbersAdapter.NumberViewHolder>(DiffC
     }
 
 
-    class NumberViewHolder(private val binding: CardNumberContainerBinding) :
+    class NumberViewHolder(
+        private val binding: CardNumberContainerBinding,
+        private val onButtonClick: (Int) -> Unit
+    ) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(numberValue: Int) {
-            binding.numberText.text = numberValue.toString()
+            binding.apply {
+                numberText.text = numberValue.toString()
+                numberButton.setOnClickListener { onButtonClick(numberValue) }
+            }
         }
     }
 
@@ -36,7 +44,7 @@ class NumbersAdapter() : ListAdapter<Int, NumbersAdapter.NumberViewHolder>(DiffC
                 LayoutInflater.from(parent.context),
                 parent,
                 false
-            )
+            ), onButtonClick
         )
     }
 
--- ListApp/app/src/main/java/com/example/listapp/fragments/NumbersListFragment.kt
+++ ListApp/app/src/main/java/com/example/listapp/fragments/NumbersListFragment.kt
@@ -1,10 +1,13 @@
 package com.example.listapp.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.listapp.R
 import com.example.listapp.adapter.NumbersAdapter
 import com.example.listapp.databinding.FragmentNumbersListBinding
 
@@ -14,19 +17,24 @@ class NumbersListFragment : Fragment() {
     private lateinit var binding: FragmentNumbersListBinding
     private lateinit var numbersAdapter: NumbersAdapter
     private val numbers: List<Int> = (1..10).toList()
+    private val TAG = this.javaClass.simpleName
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentNumbersListBinding.inflate(inflater, container, false)
-        numbersAdapter = NumbersAdapter()
+        numbersAdapter = NumbersAdapter() { numberSelected ->
+            Log.d(TAG, "number selected :$numberSelected")
+            findNavController().navigate(R.id.viewFragment)
+
+        }
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
 
         numbersAdapter.submitList(numbers)
 
